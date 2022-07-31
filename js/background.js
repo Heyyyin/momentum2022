11d3d6c7fdb24445c180ab169f7e008b746cdc3e
Heyyyin <hijung1024@gmail.com>
2022-07-31 19:51:06 +0900

    done

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images=["pexels-emiliano-arano-1298684.jpg", "pexels-flo-maderebner-869258.jpg", "pexels-james-wheeler-1519088.jpg", "pexels-pixabay-36478.jpg", "pexels-pixabay-36717.jpg", "pexels-pixabay-64219.jpg", "pexels-pixabay-417173.jpg", "pexels-pok-rie-1655166.jpg", "pexels-quang-nguyen-vinh-2131841.jpg", "pexels-stein-egil-liland-1933317.jpg", "pexels-suomi-photography-2024881.jpg","pexels-couleur-2317904.jpg","pexels-couleur-2317904.jpg","pexels-felix-mittermeier-4644813.jpg","pexels-george-desipris-792381.jpg","pexels-kelvin-valerio-617278.jpg","pexels-naushil-asari-638738.jpg","pexels-pixabay-50577.jpg","pexels-pixabay-68669.jpg","pexels-pixabay-144234.jpg","pexels-pixabay-247502.jpg"];
+
+const chosenImage= images[Math.floor(Math.random()*images.length)];
+
+const bgImage=document.createElement("img");
+bgImage.classList.add("background");
+
+bgImage.src=`img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
---
+++ js/weather.js
@@ -0,0 +1,29 @@
+const API_KEY="826ed434fc09ea07e8c78fa0b8daa1d9";
+
+function onGeoOk(position){
+    const lat=position.coords.latitude;
+    const lon=position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then((data)=>{
+        const city=document.querySelector("#weather span:first-child");
+        const weather=document.querySelector("#weather span:nth-child(2)");
+        const temperature=document.querySelector("#weather span:nth-child(3)");
+        const temperatureRange=document.querySelector("#weather span:nth-child(4)");
+        const temperatureFeelslike=document.querySelector("#weather span:nth-child(5)");
+      
+        city.innerText=data.name;
+        weather.innerText=data.weather[0].main;
+        temperature.innerText=`temperature ${data.main.temp}℃`;
+        temperatureRange.innerText=`min ${data.main.temp_min}℃ / max ${data.main.temp_max}℃`;
+        temperatureFeelslike.innerText=`It feels like ${data.main.feels_like}℃`;
+
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
